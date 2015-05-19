96e4da0f9fd82ae5658d51d176777f6dce364318
Jesse Shawl <jesse@jshawl.com>
2015-05-19 07:06:52 -0400

    add student endpoints

--- index.js
+++ index.js
@@ -3,13 +3,27 @@ var app = express()
 var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+var _ = require("underscore")
 mongoose.connect('mongodb://localhost/attendance', function(){
       //mongoose.connection.db.dropDatabase()
 });
-var Event = mongoose.model('Event', { studentId: String, weekday: String, status: String });
+var Event = mongoose.model('Event', { studentId: String, weekday: String, status: String })
+
 app.get("/", function( req, res ){
   Event.find({}, function( err, docs ){
+    res.send(docs)
+  })
+})
+
+app.get("/:weekday", function( req, res ){
+  Event.find({weekday: req.params.weekday}, function( err, docs ){
+    res.send(docs)
+  })
+})
+
+app.get("/students/:studentId", function( req, res ){
+  Event.find({studentId: req.params.studentId}, function( err, docs ){
     res.send(docs)
   })
 })
